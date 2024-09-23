7ff776b6dbc6303738e3849b17ec65c30f47cf6a
kamleshpawar2006 <kamleshpawar2006@gmail.com>
2024-09-23 14:52:54 +0100

    combined swagger and both microservice working

---
+++ src/main/java/com/tm/config/WebConfig.java
@@ -0,0 +1,29 @@
+package com.tm.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/v3/api-docs")
+                .allowedOrigins("http://localhost:9102")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false);
+
+        registry.addMapping("/tourism/api/v1/admin/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false);
+    }
+
+}
